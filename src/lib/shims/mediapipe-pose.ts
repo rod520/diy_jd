87c4356234d44315a9968b59e3f716fc1516c548
rod520 <rodriguesorlen@gmail.com>
2026-07-09 14:26:11 -0500

    wipe everything

--- src/lib/shims/mediapipe-pose.ts
+++
@@ -1,9 +0,0 @@
-export class Pose {
-	constructor() {
-		throw new Error(
-			'This Pose shim only exists so Vite can bundle @tensorflow-models/pose-detection. Use the TFJS runtime, not MediaPipe runtime.'
-		);
-	}
-}
-
-export default Pose;
